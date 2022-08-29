759fe19a325d65467bf3453e05689e86245f6edd
OOLebedenko <oolebedenko@gmail.com>
2022-08-29 16:43:42 +0300

    Add md_protocol for equilibration of production run in OPC

---
+++ md_setup/md_protocol/OPC/02_production/wt/build.sh
@@ -0,0 +1,62 @@
+#! /bin/bash -f
+
+######################################################################################################################################################################################
+#                                                   1. extract last frame from trajectory of tails equilibration  
+######################################################################################################################################################################################
+
+$AMBERHOME/bin/ambpdb -p ../../01_equil_histone_tails/wt/1_build/protein.prmtop -c ../../01_equil_histone_tails/wt/6_run/run00100.nc > 0_prepare/wt_equlibrated_tails.pdb
+
+######################################################################################################################################################################################
+#                                                   2. solvate system
+######################################################################################################################################################################################
+
+$AMBERHOME/bin/tleap -s -f 1_build/tleap_draft_box.rc
+
+
+######################################################################################################################################################################################
+#                                                   3. calculate number of Na+ and Cl- ions needed to neutralize system and to match the target salt concentration 
+######################################################################################################################################################################################
+
+# target NaCl concentration
+target_nacl_concentration=0.15  # mol/l
+
+# avogadro_constant
+avogadro_constant=6.022140857e+23 # mol-1
+
+# number of NaCl molecules in the 1 L NaCl solution
+n_nacl_in_one_liter=$(awk "BEGIN {print $target_nacl_concentration * $avogadro_constant; exit}")
+
+# The density of water is ~0.997 g/mL at the room temperature, and thus, the weight of 1 L water is ~997 g.
+# Since the weight of 1 mol H2O is ~18.02 g
+# e.g. 1 L water is composed of ~3.33 × 10^25 H2O molecules (55.3 mol).
+n_h20_in_one_liter=3.33E+25
+
+# extract number of added water molecules by tleap
+n_n20=$(grep -i "Added" leap.log | awk '{print $2}')
+
+# calcucate the number of NaCl molecules to match the target concentration
+n_nacl_target=$(awk "BEGIN {printf $n_nacl_in_one_liter / $n_h20_in_one_liter * $n_n20; exit}") 
+n_nacl_target=${n_nacl_target%.*}
+
+# extract number of added ions by tleap to neutralize system
+n_na=$(grep -i "Na+ ion" leap.log | awk '{print $1}')
+n_cl=$(grep -i "Cl- ion" leap.log | awk '{print  $1}')
+
+
+# final number of Na+ and Cl- to neutralize system and to match the target salt concentration 
+n_na=$((n_na + n_nacl_target))
+n_cl=$((n_cl + n_nacl_target))
+
+
+
+######################################################################################################################################################################################
+#                                                       3. resolvate system to match experimental NaCl concentration
+######################################################################################################################################################################################
+
+# save input file for tleap
+cp 1_build/tleap_draft_box.rc 1_build/tleap_box.rc
+sed -i s/draft_//g 1_build/tleap_box.rc
+sed -i "s/addions mol Na+ 0/addions mol Na+ ${n_na}/g" 1_build/tleap_box.rc
+sed -i "s/addions mol Cl- 0/addions mol Cl- ${n_cl}/g" 1_build/tleap_box.rc
+
+$AMBERHOME/bin/tleap -s -f 1_build/tleap_box.rc
---
+++ md_setup/md_protocol/OPC/02_production/wt/run_job.sh
@@ -0,0 +1,24 @@
+#! /bin/bash -f
+
+export CUDA_VISIBLE_DEVICES=0
+TRAJECTORY_LENGTH=2000 #ns
+
+$AMBERHOME/bin/pmemd.cuda -O -i 2_min_1/min_1.in -o 2_min_1/min_1.out -p 1_build/box.prmtop -c 1_build/box.inpcrd  -ref 1_build/box.inpcrd -r 2_min_1/min_1.ncrst -inf 2_min_1/min_1.mdinfo 
+$AMBERHOME/bin/pmemd.cuda -O -i 3_min_2/min_2.in -o 3_min_2/min_2.out -p 1_build/box.prmtop -c 2_min_1/min_1.ncrst -r 3_min_2/min_2.ncrst -inf 3_min_2/min_2.mdinfo 
+$AMBERHOME/bin/pmemd.cuda -O -i 4_heat/heat.in -o 4_heat/heat.out -p 1_build/box.prmtop -c 3_min_2/min_2.ncrst -ref 3_min_2/min_2.ncrst -r 4_heat/heat.ncrst -x 4_heat/heat.nc -inf 4_heat/heat.mdinfo 
+$AMBERHOME/bin/pmemd.cuda -O -i 5_equil/equil.in -o 5_equil/equil.out -p 1_build/box.prmtop -c 4_heat/heat.ncrst -r 5_equil/equil.ncrst -x 5_equil/equil.nc -inf 5_equil/equil.mdinfo
+$AMBERHOME/bin/pmemd.cuda -O -i 6_run/run.in  -o 6_run/run.out -p 1_build/box.prmtop -c 5_equil/equil.ncrst -r 6_run/run.ncrst -x 6_run/run.nc -inf 6_run/run.mdinfo
+$AMBERHOME/bin/pmemd.cuda -O -i 6_run/run00001.in  -o 6_run/run00001.out -p 1_build/box.prmtop -c 5_equil/equil.ncrst -r 6_run/run00001.ncrst -x 6_run/run00001.nc -inf 6_run/run00001.mdinfo
+
+# For the best control of process, the long MD trajectory consist of short simulations length of 1 ns with the same parameters
+# This makes it easy to restart the specific MD step if some technical failure has occurred.
+for (( md_step=1; md_step<=$TRAJECTORY_LENGTH - 1; md_step++ )); do 
+
+	prefix_in="run$(printf "%05g" $md_step)"
+	prefix_out="run$(printf "%05g" $((md_step + 1)))"       
+
+	$AMBERHOME/bin/pmemd.cuda -O -i "6_run/${prefix_in}.in"  -o "6_run/${prefix_out}.out" -p "1_build/box.prmtop" -c "6_run/${prefix_in}.ncrst" -r "6_run/${prefix_out}.ncrst" -x "6_run/${prefix_out}.nc" -inf "6_run/${prefix_out}.mdinfo"
+    cp "6_run/${prefix_in}.in"  "6_run/${prefix_out}.in" 
+
+done
+ 
