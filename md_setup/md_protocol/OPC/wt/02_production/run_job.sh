24bbab3eedb46262f6856ce6829690073e21f359
OOLebedenko <oolebedenko@gmail.com>
2022-09-06 12:21:42 +0300

    delete unused 6_run/run.in

--- md_setup/md_protocol/OPC/wt/02_production/run_job.sh
+++ md_setup/md_protocol/OPC/wt/02_production/run_job.sh
@@ -7,7 +7,6 @@ $AMBERHOME/bin/pmemd.cuda -O -i 2_min_1/min_1.in -o 2_min_1/min_1.out -p 1_build
 $AMBERHOME/bin/pmemd.cuda -O -i 3_min_2/min_2.in -o 3_min_2/min_2.out -p 1_build/box.prmtop -c 2_min_1/min_1.ncrst -r 3_min_2/min_2.ncrst -inf 3_min_2/min_2.mdinfo 
 $AMBERHOME/bin/pmemd.cuda -O -i 4_heat/heat.in -o 4_heat/heat.out -p 1_build/box.prmtop -c 3_min_2/min_2.ncrst -ref 3_min_2/min_2.ncrst -r 4_heat/heat.ncrst -x 4_heat/heat.nc -inf 4_heat/heat.mdinfo 
 $AMBERHOME/bin/pmemd.cuda -O -i 5_equil/equil.in -o 5_equil/equil.out -p 1_build/box.prmtop -c 4_heat/heat.ncrst -r 5_equil/equil.ncrst -x 5_equil/equil.nc -inf 5_equil/equil.mdinfo
-$AMBERHOME/bin/pmemd.cuda -O -i 6_run/run.in  -o 6_run/run.out -p 1_build/box.prmtop -c 5_equil/equil.ncrst -r 6_run/run.ncrst -x 6_run/run.nc -inf 6_run/run.mdinfo
 $AMBERHOME/bin/pmemd.cuda -O -i 6_run/run00001.in  -o 6_run/run00001.out -p 1_build/box.prmtop -c 5_equil/equil.ncrst -r 6_run/run00001.ncrst -x 6_run/run00001.nc -inf 6_run/run00001.mdinfo
 
 # For the best control of process, the long MD trajectory consist of short simulations length of 1 ns with the same parameters
